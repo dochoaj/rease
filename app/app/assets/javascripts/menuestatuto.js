6dedcf99910677adb98d062b1f9e5933b707349a
PazBustos <paz.bustos.m@gmail.com>
2015-06-16 21:37:27 -0300

    Modificaciones por reunion con profesor

---
+++ app/app/assets/javascripts/menuestatuto.js
@@ -0,0 +1,48 @@
+function estatuto1(){
+	document.getElementById('titulo1').style.display='block';
+	document.getElementById('titulo2').style.display='none';
+	document.getElementById('titulo3').style.display='none';
+	document.getElementById('titulo4').style.display='none';
+	document.getElementById('docCompleto').style.display='none';
+	document.getElementById('indice').style.display='none';
+}
+function estatuto2(){
+	document.getElementById('titulo1').style.display='none';
+	document.getElementById('titulo2').style.display='block';
+	document.getElementById('titulo3').style.display='none';
+	document.getElementById('titulo4').style.display='none';
+	document.getElementById('docCompleto').style.display='none';
+	document.getElementById('indice').style.display='none';
+}
+function estatuto3(){
+	document.getElementById('titulo1').style.display='none';
+	document.getElementById('titulo2').style.display='none';
+	document.getElementById('titulo3').style.display='block';
+	document.getElementById('titulo4').style.display='none';
+	document.getElementById('docCompleto').style.display='none';
+	document.getElementById('indice').style.display='none';
+}
+function estatuto4(){
+	document.getElementById('titulo1').style.display='none';
+	document.getElementById('titulo2').style.display='none';
+	document.getElementById('titulo3').style.display='none';
+	document.getElementById('titulo4').style.display='block';
+	document.getElementById('docCompleto').style.display='none';
+	document.getElementById('indice').style.display='none';
+}
+function estatuto5(){
+	document.getElementById('titulo1').style.display='none';
+	document.getElementById('titulo2').style.display='none';
+	document.getElementById('titulo3').style.display='none';
+	document.getElementById('titulo4').style.display='none';
+	document.getElementById('docCompleto').style.display='block';
+	document.getElementById('indice').style.display='none';
+}
+function estatuto6(){
+	document.getElementById('titulo1').style.display='none';
+	document.getElementById('titulo2').style.display='none';
+	document.getElementById('titulo3').style.display='none';
+	document.getElementById('titulo4').style.display='none';
+	document.getElementById('docCompleto').style.display='none';
+	document.getElementById('indice').style.display='block';
+}
