cf4fb95823db1bce2d8fee9f1634d292044600b5
PazBustos <paz.bustos.m@gmail.com>
2015-06-22 23:42:48 -0300

    modificaciones diseño A+S

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,26 @@
+function menu1(){
+	document.getElementById('mision').style.display='block';
+	document.getElementById('vision').style.display='none';
+	document.getElementById('valores').style.display='none';
+	document.getElementById('objetivos').style.display='none';
+}
+function menu2(){
+	document.getElementById('mision').style.display='none';
+	document.getElementById('vision').style.display='block';
+	document.getElementById('valores').style.display='none';
+	document.getElementById('objetivos').style.display='none';
+}
+function menu3(){
+	document.getElementById('mision').style.display='none';
+	document.getElementById('vision').style.display='none';
+	document.getElementById('valores').style.display='block';
+	document.getElementById('objetivos').style.display='none';
+}
+function menu4(){
+	document.getElementById('mision').style.display='none';
+	document.getElementById('vision').style.display='none';
+	document.getElementById('valores').style.display='none';
+	document.getElementById('objetivos').style.display='block';
+}
+
+
