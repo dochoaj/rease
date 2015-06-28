44b50fcb516fd6623c8c981bdc2883ecf049eaf6
Daniel Ochoa <dochoaj@gmail.com>
2015-06-28 10:25:18 -0300

    Rebuild event migration

---
+++ db/migrate/20150418063313_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+     t.references :user
+     t.string :title
+     t.text :description
+     t.text :adress
+     t.timestamps
+    end
+  end
+end
